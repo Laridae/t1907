199a040151b7686c29a766886221578a0d6baa2f
unknown <Administrator@STO-102PC04.TRAINING.local>
2020-09-15 11:11:30 +0200

    Finished BMI excercise

--- bmi_calculator/assets/js/bmi.js
+++ bmi_calculator/assets/js/bmi.js
@@ -1,27 +1,67 @@
 const BMICalculator = {
-    calculateMetric(weight, height){
-        let results = (weight/ ((height/100) * (height/100)))
-        return {
-            value: results,
-            display: this.checkResult(results)
-        }
-    },
-    checkResult(bmiValue){
-        let display = {}
-        if (bmiValue < 18.5) {
-            display.message = 'in need of Breakfast'
-            display.colour = 'Blue'
-        } else if (bmiValue >= 18.5 && bmiValue <= 24.9){
-            display.message = 'balanced'
-            display.colour = 'GoldenRod'
-        } else if (bmiValue >=25 && bmiValue <= 29.9){
-            display.message = 'absolutely fabolous'
-            display.colour = 'LightSalmon'
-        } else if (bmiValue >= 30){
-            display.message = 'in need of Hiking'
-            display.colour = 'teal'
-        }
-        return display
+  calculateMetric(weight, height) {
+    let results = weight / ((height / 100) * (height / 100));
+    return {
+      value: results,
+      display: this.checkResult2(results),
+    };
+  },
 
+  calculateImperial(weight, height) {
+    debugger
+    let results = (703 * weight / (height * height));
+    return {
+        value: results,
+        display: this.checkResult2(results),
+      };
+  },
+
+//   calculate(weight, height){
+//     switch(true) {
+//         case method === imperial:
+//     }
+//   },
+
+  checkResult(bmiValue) {
+    let display = {};
+    if (bmiValue < 18.5) {
+      display.message = "in need of Breakfast";
+      display.colour = "Blue";
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+      display.message = "balanced";
+      display.colour = "GoldenRod";
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+      display.message = "absolutely fabolous";
+      display.colour = "LightSalmon";
+    } else if (bmiValue >= 30) {
+      display.message = "in need of Hiking";
+      display.colour = "teal";
     }
-}
+    return display;
+  },
+
+  checkResult2(bmiValue) {
+    switch (true) {
+      case bmiValue < 18.5:
+        return {
+          message: "underweight",
+          colour: "red",
+        };
+      case bmiValue < 25:
+        return {
+          message: "normal",
+          colour: "green",
+        };
+      case bmiValue < 30:
+        return {
+          message: "overweight",
+          colour: "orange",
+        };
+      case bmiValue >= 30:
+        return {
+          message: "obese",
+          colour: "red",
+        };
+    }
+  },
+};
--- bmi_calculator/assets/js/bmi_ui.js
+++ bmi_calculator/assets/js/bmi_ui.js
@@ -2,16 +2,34 @@ const button = document.getElementById('calculate')
 const weightInput = document.getElementById('weight')
 const heightInput = document.getElementById('height')
 const resultsNode = document.getElementById('results')
+const selectorNode = document.getElementById('method')
+let method = 'metric'
 
-
-
-button.addEventListener('click', function() {
-    // debugger
-    let bmiValue = BMICalculator.calculateMetric(weightInput.value, heightInput.value)
-    resultsNode.innerHTML = `<h2>Your BMI value is ${bmiValue.value.toFixed(2)} - congratulations!
-    You are considered ${bmiValue.display.message}!</h2>`
+const calculateBMI = () => {
+    let bmiValue
+    if (method === 'metric'){
+        bmiValue = BMICalculator.calculateMetric(weightInput.value, heightInput.value)
+    } else {
+        bmiValue = BMICalculator.calculateImperial(weightInput.value, heightInput.value)
+    }
+    resultsNode.innerHTML = `<h2>Your BMI value is ${bmiValue.value.toFixed(2)} - congratulations! You are considered ${bmiValue.display.message}!</h2>`
     resultsNode.style.color = bmiValue.display.colour
-    
+}
+
+button.addEventListener('click', () => {
+    calculateBMI()    
+})
+
+selectorNode.addEventListener('change', () =>{
+    method = selectorNode.options[selectorNode.selectedIndex].value
+    if (method === 'imperial'){
+        weightInput.placeholder = weightInput.placeholder.replace('kilograms', 'pounds')
+        heightInput.placeholder = heightInput.placeholder.replace('centimeters', 'inches')
+    } else {
+        weightInput.placeholder = weightInput.placeholder.replace('pounds', 'kilograms')
+        heightInput.placeholder = heightInput.placeholder.replace('inches', 'centimeters')
+    }
+
 })
 
 //button.click
